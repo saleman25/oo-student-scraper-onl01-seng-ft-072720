abd55c7ab922fc4bc8c888e715d656dc584c55e7
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-16 01:36:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,10 +8,10 @@ class Scraper
     results = Nokogiri::HTML(open(index_url))
     student = []
     results.css("div.roster-cards-container").each do |card|
-    card.css(".student-card a").each do |student|
-    student_profile = "#{student.attr('href')}"
-    student_name = student.css('.student-name').text
-    student_location = student.css('.student-location').text
+    #card.css(".student-card a").each do |student|
+    #student_profile = "#{student.attr('href')}"
+    #student_name = student.css('.student-name').text
+    #student_location = student.css('.student-location').text
     student << {name: student_name, location: student_location, profile_url: student_profile}
   end
 end
