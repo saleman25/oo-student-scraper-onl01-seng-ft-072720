d719cf3ccc336c5b789c53414f7b3824768099a3
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-16 02:10:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do (attribute, value)
+    student_hash.each do |attribute, value|
     self.send("#{attribute}=", value)
   end
   @@all << self
