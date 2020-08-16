ec81544e1cef1c9d43471e700780123d594a33a1
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-16 01:20:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,8 @@ class Scraper
     results = Nokogiri::HTML(open(index_url))
     student = []
     results.css("div.roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
+    card.css(".student-card a").each do |student|
+    
     binding.pry
     results
     puts "testing pry"
