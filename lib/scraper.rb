934a1aef10d6fe7ff5c1d7293fb5dc9a6b289f0e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-16 01:04:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,18 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    results = Nokogiri::HTML(open(index_url))
+    student = []
+    results.css("student-card").first.css("a").text
+    binding.pry
+    results
+    puts "testing pry"
   end
+
 
   def self.scrape_profile_page(profile_url)
     
